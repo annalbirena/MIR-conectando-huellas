fa9b9f5c2b48d91e6643058cd6baa84719ecffd7
Anna <ana84168@gmail.com>
2024-09-26 00:56:31 -0500

    feat: add card buttons

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,5 +1,7 @@
+import { Group } from '@mantine/core';
 import AppLayout from '../components/AppLayout';
 import TitlePage from '../components/TitlePage';
+import CardButton from '../components/CardButton';
 
 function RegistrationPage() {
   return (
@@ -9,6 +11,18 @@ function RegistrationPage() {
         image="src/assets/images/pet-text.svg"
         imagePosition="right"
       />
+      <Group grow gap={100}>
+        <CardButton
+          link="/registro-mascota-perdida"
+          text="Registrar mascota perdida"
+          image="src/assets/images/lostpet.png"
+        />
+        <CardButton
+          link="/registro-mascota-adopcion"
+          text="Registrar mascota en adopción"
+          image="src/assets/images/pet.png"
+        />
+      </Group>
     </AppLayout>
   );
 }
